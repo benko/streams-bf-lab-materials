af8adafd156564b74f7b9fb93fb369b1b6406b86
Grega Bremec <gregab@p0f.net>
2024-09-12 12:25:21 +0200

    improve handling of missing logs

--- code/get-logs.sh
+++ code/get-logs.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 MYDIR=$(dirname $0)
 
+ERRORS=0
 if [ ! -e "${MYDIR}/core-api-producer/payload.log" ]; then
     echo "ERROR: Missing producer log."
-    exit 1
+    ERRORS=1
 fi
 if [ "$(echo ${MYDIR}/core-api-consumer/payload*.log)" = "${MYDIR}/core-api-consumer/payload*.log" ]; then
     echo "ERROR: Missing consumer log."
-    exit 1
+    ERRORS=1
+fi
+
+if [ ${ERRORS} -gt 0 ]; then
+    echo "WARNING: Some logs are missing, chances are that you will get inconsistent results."
+    echo "         Continue?"
+    select resp in "Y" "N"; do
+        if [ "${resp}" = "N" ]; then
+            echo "Exiting."
+            exit 0
+        elif [ "${resp}" = "Y" ]; then
+            break
+        fi
+    done
 fi
 
 if [ -e "${MYDIR}/producer.log" ] || [ -e "${MYDIR}/consumerl.log" ]; then
@@ -25,5 +39,8 @@ fi
 
 sort -gk2,3 -t, ${MYDIR}/core-api-producer/payload.log > ${MYDIR}/producer.log
 sort -gk2,3 -t, ${MYDIR}/core-api-consumer/payload*.log > ${MYDIR}/consumer.log
-rm -f ${MYDIR}/core-api-producer/payload.log ${MYDIR}/core-api-consumer/payload*.log
-echo "Producer and Consumer logs available in ${MYDIR} - old logs removed."
+
+rm -fv ${MYDIR}/core-api-producer/payload.log ${MYDIR}/core-api-consumer/payload*.log
+
+echo "Producer and/or Consumer logs available in ${MYDIR} - old logs were removed."
+
